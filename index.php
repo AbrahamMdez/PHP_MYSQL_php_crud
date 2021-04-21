5567d497141b65b7b0a20f8b8ce04f5422b62c53
Abraham <nencho_1@hotmail.com>
2021-04-21 20:03:33 +0200

    created form

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
-<?php
+<?php include('db.php') ?>
 
-echo 'Hello World';
+<?php include('includes/header.php') ?>
 
-?>
+<div class="container">
+    <div class="row p-4">
+        <div class="col-md-4">
+            <div class="card card-body">
+                <form action="save_task.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control" placeholder="Title..." autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <textarea name="description" rows="2" class="form-control" placeholder="Description..."></textarea>
+                    </div>
+
+                    <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save">
+                </form>
+            </div>
+        </div>
+        
+        <div class="col-md-8">
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php') ?>
+         
